bf2aabab221ae638fab3a7d842defa77d5af5b66
amir2015 <zahranovich2@gmail.com>
2025-05-19 22:07:33 +0300

    spec-user-auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  namespace :api, defaults: { format: :json }, constraints: { subdomain: "api" } do
+  namespace :api, defaults: { format: :json }, constraints: { subdomain: "api" }, path: "/" do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
+      resources :users, only: [:index, :show]
     end
   end
 end
