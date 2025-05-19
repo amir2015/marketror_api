1d3e386c96ad52632d0085586ca404254038434e
amir2015 <zahranovich2@gmail.com>
2025-05-19 15:49:31 +0300

    user-spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,21 @@
 require "rails_helper"
 
 RSpec.describe User, type: :model do
-  before { @user = FactoryBot.create(:user) }
+  before { @user = FactoryBot.build(:user) }
   subject { @user }
-  it { should respond_to(:email) }
-  it { should respond_to(:password) }
-  it { should respond_to(:password_confirmation) }
+
+  describe "when email is present" do
+    it { should be_valid }
+  end
+
+  describe "when responding to email,password" do
+    it { should respond_to(:email) }
+    it { should respond_to(:password) }
+    it { should respond_to(:password_confirmation) }
+  end
+  describe "when email is not present" do
+    it { should validate_uniqueness_of(:email).ignoring_case_sensitivity }
+    it { should validate_presence_of(:email) }
+    it { should validate_confirmation_of(:password) }
+  end
 end
