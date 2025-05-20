46a6f89ddc4691b15bd0085a2c17c95045e5fdef
amir2015 <zahranovich2@gmail.com>
2025-05-20 19:09:19 +0300

    user-create_spec

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -6,6 +6,21 @@ module Api
       def show
         respond_with User.find(params[:id])
       end
+
+      def create
+        user = User.new(user_params)
+        if user.save
+          render json: user, status: 201, location: [:api, user]
+        else
+          render json: { errors: user.errors }, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def user_params
+        params.require(:user).permit(:email, :password, :password_confirmation)
+      end
     end
   end
 end
