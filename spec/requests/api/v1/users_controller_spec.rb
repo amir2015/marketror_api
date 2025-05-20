2366fd08a100ef09d6a80687bc6ad7e028c6aa28
amir2015 <zahranovich2@gmail.com>
2025-05-21 01:04:31 +0300

    update-user_rspec

--- spec/requests/api/v1/users_controller_spec.rb
+++ spec/requests/api/v1/users_controller_spec.rb
@@ -40,4 +40,36 @@ RSpec.describe Api::V1::UsersController, type: :controller do
       end
     end
   end
+
+  describe "PUT/PATCH /update" do
+    context "when is successfully updated" do
+      before(:each) do
+        @user = FactoryBot.create(:user)
+        @user_attributes = FactoryBot.attributes_for(:user)
+        put :update, params: { id: @user.id, user: @user_attributes }, format: :json
+      end
+      it "returns the json for the updated user" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:email]).to eql @user_attributes[:email]
+      end
+      it { should respond_with 200 }
+    end
+    context "when is not successfully updated" do
+      before(:each) do
+        @user = FactoryBot.create(:user)
+        @user_attributes = { email: "not an email", password: "123" }
+        put :update, params: { id: @user.id, user: @user_attributes }, format: :json
+      end
+      it "returns a json error message" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response).to have_key(:errors)
+      end
+
+      it "renders json errors on the user not updated" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:errors][:email]).to include "is invalid"
+      end
+      it { should respond_with 422 }
+    end
+  end
 end
