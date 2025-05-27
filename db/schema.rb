350ab34e8b04dc083ff462803c26eccd18318e04
amir2015 <zahranovich2@gmail.com>
2025-05-28 02:14:20 +0300

    order-model_created

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_05_23_210856) do
+ActiveRecord::Schema[7.2].define(version: 2025_05_27_225029) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "orders", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.decimal "total"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
@@ -38,4 +46,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_05_23_210856) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["token"], name: "index_users_on_token", unique: true
   end
+
+  add_foreign_key "orders", "users"
 end
