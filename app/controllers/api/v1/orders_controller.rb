56daebc17d500f34c3457f1f2aa0689dc34990ac
amir2015 <zahranovich2@gmail.com>
2025-05-30 03:27:37 +0300

    show-added

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -1,9 +1,12 @@
-#rubocop:disable all
-class Api::V1::OrdersController < ApplicationController
-  before_action :authenticate_with_token,only: [:index]
+class Api::V1::OrdersController < ApplicationController # rubocop:disable Style/ClassAndModuleChildren
+  before_action :authenticate_with_token, only: [:index]
   respond_to :json
 
   def index
-    respond_with  orders: current_user.orders
+    respond_with orders: current_user.orders
+  end
+
+  def show
+    respond_with order: current_user.orders.find(params[:id])
   end
 end
--- spec/controllers/api/v1/orders_spec.rb
+++ spec/controllers/api/v1/orders_spec.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 
 require "rails_helper"
 
@@ -12,6 +13,20 @@ RSpec.describe Api::V1::OrdersController, type: :controller do
     it "returns response of 4 orders from the current-user" do
       orders_response = JSON.parse(response.body, symbolize_names: true)
       expect(orders_response[:orders].count).to eq(4)
+    end
+    it { should respond_with 200 }
+  end
+  describe "GET #show" do
+    before(:each) do
+      current_user = FactoryBot.create(:user)
+      api_authorization_header(current_user.token)
+      @order = FactoryBot.create(:order, user: current_user)
+      get :show, params: { user_id: current_user.id, id: @order.id }, format: :json
+    end
+    it "returns the order with the sent id from the current-user" do
+      json_response = JSON.parse(response.body, symbolize_names: true)
+      order_response = json_response[:order]
+      expect(order_response[:id]).to eq(@order.id)
     end
     it { should respond_with 200 }
   end
